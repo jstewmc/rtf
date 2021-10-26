37641f3dc10f8eed15b6ac46f0ddb320f6f882cc
Jack Clayton <clayjs0@gmail.com>
2021-10-26 15:26:38 -0600

    Modernize group tokens

--- src/Token/Group/Close.php
+++ src/Token/Group/Close.php
@@ -2,27 +2,10 @@
 
 namespace Jstewmc\Rtf\Token\Group;
 
-/**
- * A close group token
- *
- * @author     Jack Clayton
- * @copyright  2015 Jack Clayton
- * @license    MIT
- * @since      0.1.0
- */
-
 class Close extends Group
 {
-	/* !Public methods */
-	
-	/**
-	 * Called when the object is treated as a string
-	 *
-	 * @return  string
-	 * @since  0.2.0
-	 */
-	public function __toString()
-	{
-		return '}';
-	}
+    public function __toString(): string
+    {
+        return '}';
+    }
 }
