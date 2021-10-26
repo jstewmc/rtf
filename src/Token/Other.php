8848bd5f50a38ca3e018951c0c0f6f1562cd4438
Jack Clayton <clayjs0@gmail.com>
2021-10-26 15:27:38 -0600

    Modernize other token

--- src/Token/Other.php
+++ src/Token/Other.php
@@ -3,93 +3,26 @@
 namespace Jstewmc\Rtf\Token;
 
 /**
- * An "other" character token
- *
- * If a character does not belong to a control word, a control symbol, a group-open, 
- * a group-close, or a text token, it's an "other" character. Most often, "other"
- * tokens are carriage-returns, line-feeds, form-feeds, and null characters that are
- * ignored by the lexer.
- * 
- * @author     Jack Clayton <clayjs0@gmail.com>
- * @copyright  2015 Jack Clayton
- * @license    MIT
- * @since      0.3.0
+ * An "other" character token, which does not belong to a control word, a
+ * control symbol, a group-open, a group-close, or a text token (e.g., carriage-
+ * returns, line-feeds, form-feeds, and null characters ignored by the lexer).
  */
 class Other extends Token
 {
-	/* !Protected properties */
-	
-	/**
-	 * @var  string  the "other" character
-	 * @since  0.3.0
-	 */
-	protected $character;
-	
-	
-	/* !Public methods */
-	
-	/**
-	 * Gets the token's character
-	 *
-	 * @return  string
-	 * @since  0.3.0
-	 */
-	public function getCharacter()
-	{
-		return $this->character;
-	}
-	
-	
-	/* !Set methods */
-	
-	/**
-	 * Sets the token's character
-	 *
-	 * @param  string  $character  the "other" character
-	 * @return  self
-	 * @throws  InvalidArgumentException  if $character is not a string
-	 * @since  0.3.0
-	 */
-	public function setCharacter($character)
-	{
-		if ( ! is_string($character)) {
-			throw new \InvalidArgumentException(
-				__METHOD__."() expects parameter one, character, to be a string"
-			);
-		}
-		
-		$this->character = $character;
-		
-		return $this;
-	}
-	
-	
-	/* !Magic methods */
-	
-	/**
-	 * Called when the token is constructed
-	 *
-	 * @param  string|null  $character  the "other" character (optional; if omitted,
-	 *     defaults to null
-	 * @return  self
-	 * @throws  InvalidARgumentException  if $character is neither a string nor null
-	 */
-	public function __construct($character = null)
-	{
-		if ($character !== null) {
-			$this->setCharacter($character);
-		}
-		
-		return;
-	}
-	
-	/**
-	 * Called when the token is treated like a string
-	 *
-	 * @return  string
-	 */
-	public function __toString()
-	{
-		return "{$this->character}";
-	}
+    private string $character;
+
+    public function getCharacter()
+    {
+        return $this->character;
+    }
+
+    public function __construct(string $character)
+    {
+        $this->character = $character;
+    }
+
+    public function __toString(): string
+    {
+        return $this->character;
+    }
 }
--- tests/Token/OtherTest.php
+++ tests/Token/OtherTest.php
@@ -1,131 +1,16 @@
 <?php
-	
-use Jstewmc\Rtf\Token;
-use Jstewmc\Stream;
 
-/**
- * A test suite for the Token\Other class
- *
- * @author     Jack Clayton
- * @copyright  2015 Jack Clayton
- * @license    MIT
- * @since      0.3.0
- */
+namespace Jstewmc\Rtf\Token;
 
 class OtherTest extends \PHPUnit\Framework\TestCase
 {
-	/* !__construct() */
-	
-	/**
-	 * __construct() should return token if character does not exist
-	 */
-	public function testConstruct_returnsToken_ifCharacterDoesNotExist()
+	public function testToStringReturnsString(): void
 	{
-		$token = new Token\Other();
-		
-		$this->assertTrue($token instanceof Token\Other);
-		$this->assertNull($token->getCharacter());
-		
-		return;
+		$this->assertEquals("\n", (string)(new Other("\n")));
 	}
-	
-	/**
-	 * __construct() should return token if character does exist
-	 */
-	public function testConstruct_returnsToken_ifCharacterDoesExist()
+
+	public function testGetCharacterReturnsString(): void
 	{
-		$character = "\n";
-		
-		$token = new Token\Other($character);
-		
-		$this->assertTrue($token instanceof Token\Other);
-		$this->assertEquals($character, $token->getCharacter());
-		
-		return;
-	}
-	
-	
-	/* !__toString() */
-	
-	/**
-	 * __toString() should return string if character does not exist
-	 */
-	public function testToString_returnsString_ifCharacterDoesNotExist()
-	{
-		$token = new Token\Other();
-		
-		$this->assertEquals('', (string) $token);
-		
-		return;
-	} 
-	
-	/**
-	 * __toString() should return string if character does exist
-	 */
-	public function testToString_returnsString_ifCharacterDoesExist()
-	{
-		$character = "\n";
-		
-		$token = new Token\Other();
-		$token->setCharacter($character);
-		
-		$this->assertEquals("\n", (string) $token);
-		
-		return;
-	}
-	
-	
-	/* !getCharacter() */
-	
-	/**
-	 * getCharacter() should return null if character does not exist
-	 */
-	public function testGetCharacter_returnsNull_ifCharacterDoesNotExist()
-	{
-		$token = new Token\Other();
-		
-		$this->assertNull($token->getCharacter());
-		
-		return;
-	}
-	
-	/**
-	 * getCharacter() should return character is character does exist
-	 */
-	public function testGetCharacter_returnsCharacter_ifCharacterDoesExist()
-	{
-		$character = "\n";
-		
-		$token = new Token\Other();
-		$token->setCharacter($character);
-		
-		$this->assertEquals($character, $token->getCharacter());
-		
-		return;
-	}
-	
-	
-	/* !setCharacter() */
-	
-	/**
-	 * setCharacter() should set the token's character and return self
-	 */
-	public function testSetCharacter()
-	{
-		$character = "\n";
-		
-		$token = new Token\Other();
-		
-		$expected = $token;
-		$actual   = $token->setCharacter($character);
-		
-		$this->assertSame($expected, $actual);
-		
-		$expected = $character;
-		$actual   = $token->getCharacter();
-		
-		$this->assertEquals($expected, $actual);
-		
-		return;
+		$this->assertEquals("\n", (new Other("\n"))->getCharacter());
 	}
 }
