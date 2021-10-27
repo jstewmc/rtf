ebeb47cd62314166e8f386f1b90effe1ca40b8ee
Jack Clayton <clayjs0@gmail.com>
2021-10-27 10:15:24 -0600

    Modernize snippet

--- tests/SnippetTest.php
+++ tests/SnippetTest.php
@@ -4,113 +4,25 @@ namespace Jstewmc\Rtf;
 
 class SnippetTest extends \PHPUnit\Framework\TestCase
 {
-	/* !__construct() */
-	
-	/**
-	 * __construct() should return snippet if source does not exist
-	 */
-	public function test_read_returnsSnippet_ifSourceDoesNotExist()
+	public function testConstructReturnsSnippetWhenSourceIsEmpty(): void
 	{
-		$snippet = new Snippet();
-		
-		$this->assertTrue($snippet instanceof Snippet);
-		$this->assertEquals([], $snippet->getChildren());
-		
-		return;
+		$this->assertEquals(0, (new Snippet(''))->getLength());
 	}
-	
-	/**
-	 * __construct() should return snippet if source does exist
-	 */
-	public function test_construct_returnsSnippet_ifSourceDoesExist()
+
+	public function testConstructReturnsSnippetWhenSourceIsNotEmpty(): void
 	{
-		$snippet = new Snippet('mis\cxds ');
-		
-		$this->assertTrue($snippet instanceof Snippet);
-		$this->assertEquals(2, $snippet->getLength());
-		
-		return;
+		$this->assertEquals(2, (new Snippet('mis\cxds '))->getLength());
 	}
-	
-	
-	/* !read() */
-	
-	/**
-	 * read() should throw an InvalidArgumentException if $string is not a string
-	 */
-	public function test_read_throwsInvalidArgumentException_ifStringIsNotAString()
+
+	public function testWriteThrowsInvalidArgumentExceptionWhenFormatIsInvalid(): void
 	{
 		$this->expectException(\InvalidArgumentException::class);
-		
-		(new Snippet())->read(999);
+
+		(new Snippet(''))->write('foo');
 	}
-	
-	/**
-	 * read() should return true if $string is empty
-	 */
-	public function test_read_returnsTrue_ifStringIsEmpty()
+
+	public function testWriteReturnsStringWhenFormatIsValid(): void
 	{
-		return $this->assertTrue((new Snippet())->read(''));
-	}
-	
-	/**
-	 * read() should return true if $string is not empty
-	 */
-	public function test_read_returnsTrue_ifStringIsNotEmpty()
-	{
-		$string = '\cxds ing';
-		
-		$snippet = new Snippet();
-		
-		$expected = (new Renderer())->render(
-			(new Element\Group())
-				->appendChild(new Element\Control\Word\Cxds())
-				->appendChild(new Element\Text('ing'))
-		);
-		
-		$this->assertTrue($snippet->read($string));
-		$this->assertEquals($expected->getChildren(), $snippet->getChildren());
-		
-		return;
-	}
-	
-	
-	/* !write() */
-	
-	/**
-	 * write() should throw InvalidArgumentException if $format is not a string
-	 */
-	public function test_write_throwsInvalidArgumentException_ifFormatIsNotAString()
-	{
-		$this->expectException(\InvalidArgumentException::class);
-		
-		(new Snippet())->write(999);
-		
-		return;
-	}
-	
-	/**
-	 * write() should throw InvalidArgumentException if $format is not valid
-	 */
-	public function test_write_throwsInvalidArgumentException_ifFormatIsNotValid()
-	{
-		$this->expectException(\InvalidArgumentException::class);
-		
-		(new Snippet())->write('foo');
-		
-		return;
-	}
-	
-	/**
-	 * write() should return string if $format is valid
-	 */
-	public function test_write_returnsString_ifFormatIsValid()
-	{
-		$string = '\cxds ing';
-		
-		$snippet = new Snippet();
-		$snippet->read($string);
-		
-		return $this->assertEquals($string, $snippet->write());
+		$this->assertEquals('\cxds ing', (new Snippet('\cxds ing'))->write());
 	}
 }
