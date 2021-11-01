21bb9d43374b377fad332b8e0fe3ef1ebfdb187a
Jack Clayton <clayjs0@gmail.com>
2021-11-01 13:53:32 -0600

    Add notes to testable implementations

--- tests/Element/Control/TestControl.php
+++ tests/Element/Control/TestControl.php
@@ -2,7 +2,11 @@
 
 namespace Jstewmc\Rtf\Element\Control;
 
+/**
+ * An implementation to test the abstract class, instead of testing the same
+ * methods in all child classes.
+ */
 class TestControl extends Control
 {
-    
+
 }
--- tests/Element/TestElement.php
+++ tests/Element/TestElement.php
@@ -2,6 +2,10 @@
 
 namespace Jstewmc\Rtf\Element;
 
+/**
+ * An implementation to test the abstract class, instead of testing the same
+ * methods in all child classes.
+ */
 class TestElement extends Element
 {
 
