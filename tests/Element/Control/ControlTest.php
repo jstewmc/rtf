b774a4ce850c10f953bbfb474915ee73d443c727
Jack Clayton <clayjs0@gmail.com>
2021-11-02 16:44:30 -0600

    Finish cleaning up control, element, and group tests

--- tests/Element/Control/ControlTest.php
+++ tests/Element/Control/ControlTest.php
@@ -2,30 +2,17 @@
 
 namespace Jstewmc\Rtf\Element\Control;
 
-/**
- * A test suite for the control class
- *
- * @author     Jack Clayton
- * @copyright  2015 Jack Clayton
- * @license    MIT
- * @since      0.1.0
- */
-
 class ControlTest extends \PHPUnit\Framework\TestCase
 {
-    /* setIsSpaceDelimited() / getIsSpaceDelimited() */
+    public function testGetIsSpaceDelimitedReturnsBool(): void
+    {
+        $this->assertTrue((new TestControl())->getIsSpaceDelimited());
+    }
 
-    /**
-     * setIsSpaceDelimited() and getIsSpaceDelimited() should set and get the
-     *     flag, respectively
-     */
-    public function testSetGetIsSpaceDelimited()
+    public function testSetIsSpaceDelimitedReturnsSelf(): void
     {
         $control = new TestControl();
-        $control->setIsSpaceDelimited(true);
 
-        $this->assertTrue($control->getIsSpaceDelimited());
-
-        return;
+        $this->assertSame($control, $control->setIsSpaceDelimited(false));
     }
 }
