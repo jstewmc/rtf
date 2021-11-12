c8f79851aaf1a562bc13eadfd6d27683cb0b9802
Jack Clayton <clayjs0@gmail.com>
2021-11-12 09:54:28 -0700

    Add font table element

---
+++ src/Element/HeaderTable/HeaderTable.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Jstewmc\Rtf\Element\HeaderTable;
+
+use Jstewmc\Rtf\Element\Group;
+
+/**
+ * A group in the header with data about fonts, colors, lists, etc that can be
+ * referenced throughout a document. A header table's HTML is typically CSS, and
+ * it does not contain document text.
+ */
+abstract class HeaderTable extends Group
+{
+    public function toHtml(): string
+    {
+        return '';
+    }
+
+    public function toText(): string
+    {
+        return '';
+    }
+}
